3fa6be7d00ede4c2748e266d4715b0aace72c276
JeloSobremisana <jelony.sobremisana@gmail.com>
2022-10-14 21:51:26 +0800

    Change host on dev env

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const config = require('../config/index');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   //  default data for landing page
-  const url = `${config.HOST}:${config.PORT}/todo/get?page=${req.query.page}&limit=${req.query.limit}`;
+  const url = `${config.HOST}/todo/get?page=${req.query.page}&limit=${req.query.limit}`;
   const result = await axios.get(url)
     .then(response => {
       return response.data
